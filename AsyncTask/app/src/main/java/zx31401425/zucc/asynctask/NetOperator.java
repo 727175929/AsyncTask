635199a1a4fc32366271e1b68ea9590457f9b145
727175929 <727175929@qq.com>
2017-07-09 14:27:52 +0800

    no message

---
+++ AsyncTask/app/src/main/java/zx31401425/zucc/asynctask/NetOperator.java
@@ -0,0 +1,18 @@
+package zx31401425.zucc.asynctask;
+
+/**
+ * Created by 赵轩 on 2017/7/9.
+ */
+//模拟网络环境
+public class NetOperator {
+
+    public void operator() {
+        try {
+            //休眠1秒
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+}
